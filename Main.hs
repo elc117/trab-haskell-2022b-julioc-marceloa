6c2a5ffe3d48cb781e7fd077bbb6a2e5a7a7a4f3
Marcelo Da Silva De Da <42869269+mdazevedo96@users.noreply.github.com>
2023-01-21 00:12:00 +0000

    Testando funções

--- Main.hs
+++ Main.hs
@@ -8,3 +8,22 @@ svgRect :: Rect -> String -> String
 svgRect (x, y, w, h) style = 
       printf "<rect x=50 y=50 width= 50 height=100 style='%s' />\n" x y w h style
 
+svgBegin :: Int -> Int -> String
+svgBegin w h = 
+  printf "<svg width='%d' height='%d' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+
+svgEnd :: String
+svgEnd = "</svg>"
+
+svgStyle :: (Int,Int,Int) -> String
+svgStyle (r,g,b) = printf "fill:rgb(%d,%d,%d); mix-blend-mode: screen;" r g b
+
+
+main :: IO ()
+main = do
+  putStrLn "Check output in output.svg"
+
+svgstrings = svgBegin w h ++ svgRect ++ svgEnd in writeFile "output.svg" svgstrings
+
+    
+
